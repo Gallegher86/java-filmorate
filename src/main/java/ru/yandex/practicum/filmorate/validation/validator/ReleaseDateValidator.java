b2dbe4746eb6bf3259807be057d1d026cbe6dd2e
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-10-03 03:00:55 +0700

    Создать кастомную аннотацию ValidReleaseDate.

---
+++ src/main/java/ru/yandex/practicum/filmorate/validation/validator/ReleaseDateValidator.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.validation.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import ru.yandex.practicum.filmorate.validation.annotation.ValidReleaseDate;
+
+import java.time.LocalDate;
+
+public class ReleaseDateValidator implements ConstraintValidator<ValidReleaseDate, LocalDate> {
+    private static final LocalDate START_DATE = LocalDate.of(1895, 12, 28);
+
+    @Override
+    public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
+        return !value.isBefore(START_DATE);
+    }
+}
