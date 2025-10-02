4d8c5ba058cd68f6bbf75b849b4c5ef3ec9a73a1
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-10-03 03:25:10 +0700

    Создать GlobalExceptionHandler.

---
+++ src/main/java/ru/yandex/practicum/filmorate/config/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package ru.yandex.practicum.filmorate.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+     @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("errorMessage", ex.getMessage());
+        body.put("errorCode", HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleInvalidArgumentException(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new HashMap<>();
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+        body.put("errorMessage", "Выявлены следующие ошибки валидации:");
+        body.put("details", errors);
+        body.put("errorCode", HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleUncaughtException(Exception ex) {
+        ex.printStackTrace();//логгировать
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("errorMessage", "Произошла ошибка на сервере.");
+        body.put("errorCode", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    }
+}
