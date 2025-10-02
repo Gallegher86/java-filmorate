e80b36512dc39820dbd6e4ea8b98ec0a41d121fb
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-10-03 04:32:25 +0700

    Добавить возвращение login при пустом name в User.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -29,4 +29,8 @@ public class User {
     private String name;
     @PastOrPresent(message = "Дата рождения не может быть в будущем.")
     private LocalDate birthday;
+
+    public String getName() {
+        return (name == null || name.isBlank()) ? login : name;
+    }
 }
