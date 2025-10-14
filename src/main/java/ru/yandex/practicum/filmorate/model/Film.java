deed81ad6ffed874e4df7cefbc278d296ec16471
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-10-15 00:37:49 +0700

    feat: добавить поля friends и likes в User и Film.

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,10 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.Builder;
-import lombok.ToString;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
@@ -12,6 +8,8 @@ import org.hibernate.validator.constraints.Length;
 import ru.yandex.practicum.filmorate.validation.annotation.ValidReleaseDate;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Builder (toBuilder = true)
 @Getter
@@ -28,5 +26,21 @@ public class Film {
     private LocalDate releaseDate;
     @Positive (message = "Продолжительность фильма должна быть положительной.")
     private int duration;
+    @Builder.Default
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private final Set<Long> likes = new HashSet<>();
+
+    public void addLikeId(Long id) {
+        likes.add(id);
+    }
+
+    public void removeLikeId(Long id) {
+        likes.remove(id);
+    }
+
+    public Set<Long> getLikes() {
+        return new HashSet<>(likes);
+    }
 }
 
