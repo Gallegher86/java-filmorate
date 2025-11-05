ee6d85a18682fa4dc31da43b440c54c657b73cfa
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-11-05 21:02:59 +0700

    fix: правки стиля.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,9 +8,6 @@ import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Builder (toBuilder = true)
 @Getter
