b0f2e501c2199b726208eace7aef597b0ef93579
Anton Riabushkin <riabushkinanton@yandex.ru>
2025-11-05 18:50:20 +0700

    feat: внедрить ErrorResponse.class.

--- src/main/java/ru/yandex/practicum/filmorate/config/GlobalExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/config/GlobalExceptionHandler.java
@@ -13,29 +13,28 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 import ru.yandex.practicum.filmorate.exceptions.GenreNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.MpaNotFoundException;
 import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private static final String MESSAGE = "errorMessage";
-    private static final String CODE = "errorCode";
-    private static final String DETAILS = "details";
-
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
         log.warn("Ресурс не найден: {}", ex.getMessage());
 
-        Map<String, Object> body = makeBody(ex.getMessage(), HttpStatus.NOT_FOUND.value(), null);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(ex.getMessage())
+                .errorCode(HttpStatus.NOT_FOUND.value())
+                .build();
+
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, Object>> handleInvalidArgumentException(MethodArgumentNotValidException ex) {
-        String errorMessage = "Выявлены следующие ошибки валидации:";
+    public ResponseEntity<ErrorResponse> handleInvalidArgumentException(MethodArgumentNotValidException ex) {
+        String errorMessage = "Выявлены ошибки валидации.";
 
         List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
@@ -45,83 +44,104 @@ public class GlobalExceptionHandler {
 
         log.warn("Валидация не пройдена ({}): {}.", ex.getClass().getSimpleName(), errors);
 
-        Map<String, Object> body = makeBody(errorMessage, HttpStatus.BAD_REQUEST.value(), errors);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(errorMessage)
+                .errorCode(HttpStatus.BAD_REQUEST.value())
+                .details(errors)
+                .build();
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(IllegalArgumentException ex) {
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
         log.warn(ex.getMessage());
 
-        Map<String, Object> body = makeBody(ex.getMessage(), HttpStatus.BAD_REQUEST.value(), null);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(ex.getMessage())
+                .errorCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleWrongPath(NoResourceFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleWrongPath(NoResourceFoundException ex) {
         String errorMessage = "Ресурс по указанному пути не найден.";
         String logMessage = String.format("Получен запрос на несуществующий путь %s.", ex.getResourcePath());
         log.warn(logMessage);
 
-        Map<String, Object> body = makeBody(errorMessage, HttpStatus.NOT_FOUND.value(), null);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(errorMessage)
+                .errorCode(HttpStatus.NOT_FOUND.value())
+                .build();
+
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<Map<String, Object>> handleWrongRequestMethod(HttpRequestMethodNotSupportedException ex) {
+    public ResponseEntity<ErrorResponse> handleWrongRequestMethod(HttpRequestMethodNotSupportedException ex) {
         String errorMessage = String.format("Метод %s не поддерживается.", ex.getMethod());
         String logMessage = String.format("Получен запрос с нереализованным методом %s.", ex.getMethod());
         log.warn(logMessage);
 
-        Map<String, Object> body = makeBody(errorMessage, HttpStatus.METHOD_NOT_ALLOWED.value(), null);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(errorMessage)
+                .errorCode(HttpStatus.METHOD_NOT_ALLOWED.value())
+                .build();
+
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(body);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Map<String, Object>> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
         String name = ex.getName();
         String value = String.valueOf(ex.getValue());
         String errorMessage = String.format("Неверный формат параметра запроса '%s': '%s'. Ожидается число.", name, value);
         log.warn(errorMessage);
 
-        Map<String, Object> body = makeBody(errorMessage, HttpStatus.BAD_REQUEST.value(), null);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(errorMessage)
+                .errorCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 
     @ExceptionHandler(MpaNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleMpaNotFoundException(MpaNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleMpaNotFoundException(MpaNotFoundException ex) {
         log.warn(ex.getMessage());
 
-        Map<String, Object> body = makeBody(ex.getMessage(), HttpStatus.NOT_FOUND.value(), null);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(ex.getMessage())
+                .errorCode(HttpStatus.NOT_FOUND.value())
+                .build();
+
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 
     @ExceptionHandler(GenreNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleGenreNotFoundException(GenreNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleGenreNotFoundException(GenreNotFoundException ex) {
         log.warn(ex.getMessage());
 
-        Map<String, Object> body = makeBody(ex.getMessage(), HttpStatus.NOT_FOUND.value(), null);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(ex.getMessage())
+                .errorCode(HttpStatus.NOT_FOUND.value())
+                .build();
+
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleUncaughtException(Exception ex) {
+    public ResponseEntity<ErrorResponse> handleUncaughtException(Exception ex) {
         String errorMessage = "Произошла ошибка на сервере.";
         log.error("Необработанное исключение: {} - {}", ex.getClass().getSimpleName(), ex.getMessage(), ex);
 
-        Map<String, Object> body = makeBody(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR.value(), null);
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(errorMessage)
+                .errorCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .build();
+
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
-    }
-
-    private Map<String, Object> makeBody(String message, int status, List<String> details) {
-        Map<String, Object> body = new HashMap<>();
-        body.put(MESSAGE, message);
-
-        if (details != null) {
-            body.put(DETAILS, details);
-        }
-
-        body.put(CODE, status);
-        return body;
     }
 }
